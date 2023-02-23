efb15c37bed7271b4e98011820d50d6cfbad890f
maarxx <chris.lewark@gmail.com>
2023-02-23 08:02:36 -0500

    Formatting for Comparison

--- BeatingsContinue/JobDriver_BeatAttack.cs
+++ BeatingsContinue/JobDriver_BeatAttack.cs
@@ -17,29 +17,22 @@ namespace BeatingsContinue
             yield return Toils_General.DoAtomic(delegate
             {
                 Pawn pawn = job.targetA.Thing as Pawn;
-                //if (pawn != null && pawn.Downed && base.pawn.mindState.duty != null && base.pawn.mindState.duty.attackDownedIfStarving && base.pawn.Starving())
-                //{
-                job.killIncappedTarget = true;
-                //}
+                if (true)
+                {
+                    job.killIncappedTarget = true;
+                }
             });
             yield return Toils_Misc.ThrowColonistAttackingMote(TargetIndex.A);
             yield return Toils_Combat.FollowAndMeleeAttack(TargetIndex.A, delegate
             {
                 Thing thing = job.GetTarget(TargetIndex.A).Thing;
-                //Pawn p;
-                //if (job.reactingToMeleeThreat && (p = (thing as Pawn)) != null && !p.Awake())
-                //{
-                //    EndJobWith(JobCondition.InterruptForced);
-                //}
-                InteractionUtility.TryGetRandomVerbForSocialFight(base.pawn, out Verb verb);
                 if (BeatDecider.shouldStopBeating(base.pawn, base.TargetA.Thing as Pawn))
                 {
                     EndJobWith(JobCondition.Succeeded);
                 }
-                else
-                {
-                    pawn.meleeVerbs.TryMeleeAttack(thing, verb);
-                }
+                InteractionUtility.TryGetRandomVerbForSocialFight(base.pawn, out Verb verb);
+                pawn.meleeVerbs.TryMeleeAttack(thing, verb);
+
             }).FailOnDespawnedOrNull(TargetIndex.A);
         }
     }
